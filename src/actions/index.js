0b93f81a33a49156df65a7fae2416b8e39b0ff69
Sarah Tatro <statro09@gmail.com>
2018-03-03 16:01:24 -0700

    Initial weather app tutorial

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = '9ea15f3cbea8665f8b5d233175ab7e76';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
